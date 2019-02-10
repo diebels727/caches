08ed4573494bada5b2884b0a5dcfb2928c5cba5d
Your Name <you@example.com>
2019-02-10 10:24:43 -0500

    Add some functionality.

--- main.py
+++ main.py
@@ -1,41 +1,76 @@
 import tornado.ioloop
 import tornado.web
+import http
 import json
 
-global CACHE_PARTS
-CACHE_PARTS = {}
 
-class CachesHandler(tornado.web.RequestHandler):
+CACHE_PARTS = dict()
+
+
+def serializer(value):
+  if isinstance(value, set):
+    return list(value)
+  return value
+
+
+class Handler(tornado.web.RequestHandler):
+  def complete(self, status, message='', data=None):
+    if not data:
+      data = {}
+    self.set_status(status)
+    self.write(json.dumps({
+      'metadata': {
+        'status': status,
+        'message': message
+      },
+      'data': data
+    }, indent=2, default=serializer))
+
+
+class CachesHandler(Handler):
 
   def get(self, cache_id, part_id):
-    if part_id in CACHE_PARTS.get(cache_id, []):
-        self.set_status(200)
+    if part_id in CACHE_PARTS.get(cache_id, set()):
+        self.complete(http.HTTPStatus.OK)
         return
-    self.set_status(404)
+    self.complete(http.HTTPStatus.NOT_FOUND)
 
   def post(self, cache_id, part_id):
-    cache = CACHE_PARTS.setdefault(cache_id, [])
-    if part_id in cache:
-      self.set_status(409)
+    cache_part = CACHE_PARTS.setdefault(cache_id, set())
+    if part_id in cache_part:
+      self.complete(http.HTTPStatus.CONFLICT)
       return
-    cache.append(part_id)
-    self.set_status(200)
-    return
+    cache_part.update(part_id)
+    self.complete(http.HTTPStatus.CREATED)
+
+
+class CacheHandler(Handler):
 
   def delete(self, cache_id):
-    del CACHE_PARTS[cache_id]
-    self.set_status(200)
-    return
+    try:
+      del CACHE_PARTS[cache_id]
+    except:
+      self.complete(http.HTTPStatus.INTERNAL_SERVER_ERROR)
+      return
+    self.complete(http.HTTPStatus.OK)
 
-class StatusHandler(tornado.web.RequestHandler):
+
+class StatusHandler(Handler):
+
   def get(self):
-    self.write(json.dumps(CACHE_PARTS))
-    self.set_status(200)
-    return
+    global CACHE_PARTS
+    self.complete(http.HTTPStatus.OK, data=CACHE_PARTS)
+
+  def delete(self):
+    global CACHE_PARTS
+    # invalidate the cache
+    CACHE_PARTS = dict()
+    self.complete(http.HTTPStatus.OK)
 
 def make_app():
   return tornado.web.Application([
     (r"/status/?", StatusHandler),
+    (r"/cache/(.*)/?", CacheHandler),
     (r"/caches/(.*)/parts/(.*)/?", CachesHandler),
   ])
 
